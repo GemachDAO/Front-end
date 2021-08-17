69cd82dc038d9bc335a717dc14ad13f264cf6c85
nbunruh <nolanbunruh@gmail.com>
2021-08-16 20:42:26 -0700

    updated interest tab link

--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -78,7 +78,7 @@ const Header = ({ currentPage = '' }) => {
         </HeaderBrandContainer>
         <HeaderNavContainer>
           <HeaderNav active={isMobileNavActive}>
-            <NextLink href="https://stake.gemach.io/" passHref={true}>
+            <NextLink href="https//stake.gemach.io/" passHref={true}>
               <HeaderNavItem
                 target="_blank"
                 onClick={() => setMobileNavIsActive(false)}
